5a9effe0564d01c8ff92e56dc110010e371363f8
Nayara Vasconcelos <nayaravasconcelos.dev@gmail.com>
2023-02-16 21:46:43 -0300

    feat: configura app

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,12 @@
 import express from 'express';
+import 'express-async-errors';
+import ErrorHandler from './Middlewares/ErrorHandler';
+import routes from './Routes';
 
 const app = express();
+
+app.use(express.json());
+app.use(routes);
+app.use(ErrorHandler.handle);
 
 export default app;
