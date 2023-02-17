4d5c7514020e4bbefeb5ac79e9e7ac2d4d76f1e6
Nayara Vasconcelos <nayaravasconcelos.dev@gmail.com>
2023-02-16 21:50:28 -0300

    test: verifica se pode criar carros

---
+++ tests/unit/Services/01-create-car.test.ts
@@ -0,0 +1,57 @@
+import { expect } from 'chai';
+import sinon from 'sinon';
+import { Model } from 'mongoose';
+import ICar from '../../../src/Interfaces/ICar';
+import Car from '../../../src/Domains/Car';
+import CarService from '../../../src/Services/CarService';
+
+describe('Deve ser possível criar carros', function () {
+  it('Adiciona carro com sucesso', async function () {
+    const carInput: ICar = {
+      model: 'Marea',
+      year: 1992,
+      color: 'Red',
+      status: true,
+      buyValue: 12.000,
+      doorsQty: 2,
+      seatsQty: 5,
+    };
+
+    const carMock: ICar = {
+      _id: '634852326b35b59438fbea2f',
+      model: 'Marea',
+      year: 1992,
+      color: 'Red',
+      status: true,
+      buyValue: 12.000,
+      doorsQty: 2,
+      seatsQty: 5,
+    };
+
+    const expectedOutput: Car = new Car({
+      _id: '634852326b35b59438fbea2f',
+      model: 'Marea',
+      year: 1992,
+      color: 'Red',
+      status: true,
+      buyValue: 12.000,
+      doorsQty: 2,
+      seatsQty: 5,
+    });
+
+    sinon.stub(Model, 'create').resolves(carMock);
+
+    const carService = new CarService();
+    const result = await carService.create(carInput);
+
+    expect(carService.create).not.to.throw();
+    expect(result).to.be.an('object');
+    expect(Object.keys(result)).to.have.lengthOf(8);
+    expect(result).to.have
+      .all.keys('id', 'model', 'year', 'color', 'status', 'buyValue', 'doorsQty', 'seatsQty');
+    expect(result).to.be.deep.equal(expectedOutput);
+    expect(result).to.an.instanceof(Car);
+
+    sinon.restore();
+  });
+});
