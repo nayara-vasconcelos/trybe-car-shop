11b4b060a64f16fdade52c963d07373bc231a23e
Nayara Vasconcelos <nayaravasconcelos.dev@gmail.com>
2023-02-16 21:09:48 -0300

    feat: adiciona metodo create em CarController

---
+++ src/Controllers/CarController.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from 'express';
+import HttpStatus from '../Enums/httpStatus';
+import ICar from '../Interfaces/ICar';
+
+import ICarService from '../Interfaces/ICarService';
+
+export default class CarController {
+  private readonly _carService: ICarService;
+
+  constructor(CarService: ICarService) {
+    this._carService = CarService;
+  }
+
+  public create = async (req: Request, res: Response) => {
+    const { model, year, color, status, buyValue, doorsQty, seatsQty } = req.body;
+    const car = await this._carService.create(
+      { model, year, color, status, buyValue, doorsQty, seatsQty } as ICar,
+    );
+
+    return res.status(HttpStatus.CREATED).json(car);
+  };
+}
