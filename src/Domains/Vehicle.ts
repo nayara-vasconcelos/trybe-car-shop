279dc5dab7991ffb1cade8665def087e48f5d77e
Nayara Vasconcelos <nayaravasconcelos.dev@gmail.com>
2023-02-16 21:03:01 -0300

    feat: adiciona classe de dominio para veiculos

---
+++ src/Domains/Vehicle.ts
@@ -0,0 +1,21 @@
+import IVehicle from '../Interfaces/IVehicle';
+
+export default abstract class Vehicle {
+  protected id?: string;
+  protected model: string;
+  protected year: number;
+  protected color: string;
+  protected status?: boolean;
+  protected buyValue: number;
+
+  constructor(vehicle: IVehicle) {
+    if (vehicle._id) {
+      this.id = vehicle._id;
+    }
+    this.model = vehicle.model;
+    this.year = vehicle.year;
+    this.color = vehicle.color;
+    this.status = vehicle.status;
+    this.buyValue = vehicle.buyValue;
+  }
+}
