c599c5ed3525634a4ec3c0200c1ce5987827275d
Nayara Vasconcelos <nayaravasconcelos.dev@gmail.com>
2023-02-26 13:09:58 -0300

    feat: atualiza classe que valida campos
    Corrige erro de escrita no nome da classe e adiciona o metodo static
    updateCar para validar corpo da requisicao para atualizar carro.

--- src/Middlewares/FieldsValidator.ts
+++ src/Middlewares/FieldsValidator.ts
@@ -2,8 +2,13 @@ import { NextFunction, Request, Response } from 'express';
 import ICar from '../Interfaces/ICar';
 import CarValidator from '../utils/CarValidator';
 
-export default class FiledsValidator {
+export default class FieldsValidator {
   public static createCar = (req: Request, _res: Response, next: NextFunction) => {
+    new CarValidator(req.body as ICar).validateAll();
+    next();
+  };
+
+  public static updateCar = (req: Request, _res: Response, next: NextFunction) => {
     new CarValidator(req.body as ICar).validateAll();
     next();
   };
