93bf970aa5874a96ce2ec73b70d2138f12c37106
Nayara Vasconcelos <nayaravasconcelos.dev@gmail.com>
2023-02-28 21:33:28 -0300

    feat: adiciona middleware para validar campos
    Adiciona classe para verificar campos das
    requisicoes e implementa
    metodo static para validar os campos do corpo da requicisao
    para registrar uma moto.

--- src/Middlewares/FieldsValidator.ts
+++ src/Middlewares/FieldsValidator.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from 'express';
 import ICar from '../Interfaces/ICar';
+import IMotorcycle from '../Interfaces/IMotorcycle';
 import CarValidator from '../utils/CarValidator';
+import MotorcycleValidator from '../utils/MotorcycleValidator';
 
 export default class FieldsValidator {
   public static createCar = (req: Request, _res: Response, next: NextFunction) => {
@@ -10,6 +12,11 @@ export default class FieldsValidator {
 
   public static updateCar = (req: Request, _res: Response, next: NextFunction) => {
     new CarValidator(req.body as ICar).validateAll();
+    next();
+  };
+
+  public static createMotorcycle = (req: Request, _res: Response, next: NextFunction) => {
+    new MotorcycleValidator(req.body as IMotorcycle).validateAll();
     next();
   };
 }
