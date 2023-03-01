9f462a68d27b117977b8de59d1fa9a40e6d95f75
Nayara Vasconcelos <nayaravasconcelos.dev@gmail.com>
2023-02-28 21:59:40 -0300

    test: verifica se e possivel registrar uma moto

---
+++ tests/unit/Services/05-create-motorcycle.test.ts
@@ -0,0 +1,48 @@
+import { expect } from 'chai';
+import sinon from 'sinon';
+import { Model } from 'mongoose';
+import IMotorcycle from '../../../src/Interfaces/IMotorcycle';
+import Motorcycle from '../../../src/Domains/Motorcycle';
+import MotorcycleService from '../../../src/Services/MotorcycleService';
+
+describe('Deve ser possível criar carros', function () {
+  it('Adiciona carro com sucesso', async function () {
+    const motorcycleInput: IMotorcycle = {
+      model: 'Honda Cb 600f Hornet',
+      year: 2005,
+      color: 'Yellow',
+      status: true,
+      buyValue: 30.000,
+      category: 'Street',
+      engineCapacity: 600,
+    };
+
+    const motorcycleMock: IMotorcycle = {
+      id: '6348513f34c397abcad040b2',
+      model: 'Honda Cb 600f Hornet',
+      year: 2005,
+      color: 'Yellow',
+      status: true,
+      buyValue: 30.000,
+      category: 'Street',
+      engineCapacity: 600,
+    };
+
+    const expectedOutput: Motorcycle = new Motorcycle(motorcycleMock);
+
+    sinon.stub(Model, 'create').resolves(motorcycleMock);
+
+    const motorcycleService = new MotorcycleService();
+    const result = await motorcycleService.create(motorcycleInput);
+
+    expect(motorcycleService.create).not.to.throw();
+    expect(result).to.be.an('object');
+    expect(Object.keys(result)).to.have.lengthOf(8);
+    expect(result).to.have
+      .all.keys('id', 'model', 'year', 'color', 'status', 'buyValue', 'category', 'engineCapacity');
+    expect(result).to.be.deep.equal(expectedOutput);
+    expect(result).to.an.instanceof(Motorcycle);
+
+    sinon.restore();
+  });
+});
